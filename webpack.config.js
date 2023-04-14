afbb15c643dc86ee127710a7ee4987b13140f91c
Sarah Reimann <reimannsarah45@gmail.com>
2023-04-14 08:47:56 -0700

    update name in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
