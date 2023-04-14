bcc4d13543e0e96d8b03edfa7d86679290335663
Sarah Reimann <reimannsarah45@gmail.com>
2023-04-14 09:17:37 -0700

    writetest to calculate age in venus years

--- __tests__ /age.test.js
+++ __tests__ /age.test.js
@@ -15,4 +15,8 @@ describe('EarthAge', () => {
   test('should correctly calculate mercury age', ()=> {
     expect(earthAge.calculateMercuryAge(34)).toEqual(141.6667);
   });
+
+  test('should correctly calculate venus age', () => {
+    expect(earthAge.calculateVenusAge(34)).toEqual(54.8387);
+  });
 });
