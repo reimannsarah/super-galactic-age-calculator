d5d161493ba1a73261cacfca2f4fb456c22874d5
Sarah Reimann <reimannsarah45@gmail.com>
2023-04-14 08:56:40 -0700

    write test to calculate mercury age

--- __tests__ /age.test.js
+++ __tests__ /age.test.js
@@ -2,8 +2,17 @@ import EarthAge from './../src/js/age.js';
 
 describe('EarthAge', () => {
 
+  let earthAge;
+
+  beforeEach(() => {
+    earthAge = new EarthAge(34)
+  });
+
   test('should correctly create an age object with one property of age', () => {
-    let earthAge = new EarthAge(34);
     expect(earthAge.age).toEqual(34);
+  });
+
+  test('should correctly calculate mercury age', ()=> {
+    expect(earthAge.calculateMercuryAge()).toEqual(34);
   });
 });
